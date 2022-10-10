8515c96f096365dd1b2ff7ad20037374d57792a6
Zarina1708 <115084290+Zarina1708@users.noreply.github.com>
2022-10-11 01:10:20 +0600

    DZ3

--- main.js
+++ main.js
@@ -1,13 +1,38 @@
 // задача 1
-//введите столбец чисел от 1 до 50, при помощи циклов while и for
-let num = 1;
-while (num < 51) {
-    console.log(num);
-    num++;
+// если переменная А больше нуля-то в GGG запишем функцию, которая выводит один !, иначе запишем функцию, которая выводит два !.
+
+var a;
+if ( a > 0 ) {
+    var ggg = function () {
+        console.log("!");
+    }
+}
+else {
+    var ggg = function () {
+        console.log("!!");
+    }
 }
 
 // задача 2
-// дан массив с элементами 'Привет,', 'мир' и '!'. Необходимо вывести на экран фразу 'ПРИВЕТ, МИР!'.
+// заполните массив 10-ю иксами с помощью цикла
 
-var arr = ['Привет,', 'мир', '!'];
-alert(arr[0] + arr[1] + arr[2]);
+var arr = [];
+for (var i = 1; i <= 10; i++) {
+    arr[i] = 'x';
+}
+console.log(arr);
+
+// задача 3
+// сделайте функцию, котрая считает и выводит количество своих вызовов.
+
+function counter() {
+    var i = 1;
+    return function () {
+        return i++
+    };
+}
+var func = counter();
+console.log(func());
+console.log(func());
+console.log(func());
+console.log(func());
