3c3ac8b24bac5ff34a75de85ce711ea3e4ee223f
Zarina1708 <115084290+Zarina1708@users.noreply.github.com>
2022-10-29 10:16:15 +0600

    dz 8

--- app.js
+++ app.js
@@ -1,63 +1,114 @@
-// №1 реализуйте класс Worker (работник), который будет иметь следующие свойства: name (имя), surname(фамилия), rate(ставка за день работы),
-// days(количество отработанных дней). Также класс должен иметь метод getSalary(), который будет выводить зарплату работника.
-// Зарплата-это произведение (умножение) ставки rate на количество отработанных дней days.
+document.querySelector("#generate").addEventListener("click", () => {
+  generate();
+});
+
+generate = () => {
+  var quotes = {
+        "- ZZ":
+          '"Ясно только солнце, все остальное вообще не ясно."',
+        "- ZZ":
+          '"Любите искусство в себе, а не себя в искусстве."',
+        "- ZZ":
+          '"Сам себе патронус."',
+        "- ZZ":
+          '"И пусть все сложное станет простым."',
+        "- ZZ":
+          '"Люблю запах возможностей по утром."',
+        "- ZZ":
+          '"Даже если я ошибаюсь, дай мне ошибиться."',
+        "- ZZ":
+          '"Каждый бережет тебя настолько, насколько нуждается в тебе."',
+        "- ZZ":
+          '"В мире полном разочарований, оставайся моим светом."',
+        "- ZZ": '"Не останавливайся, пока не будешь собой гордиться. "',
+        "- ZZ":
+          '"Цените свой покой больше, чем мнение людей."',
+        "- ZZ":
+          '"Пусть будет у вас человек из-за которого хочется меньше сидеть в телефоне."',
+        "- ZZ":
+          '"Где интерес, там и энергия."',
+        "- ZZ":
+          '"Порою нужен сбой в системе, и шаг на ощупь в темноте...  А иногда, побыть не с теми, чтобы, наконец, понять, кто те. "',
+
+ };
+
+  var authors = Object.keys(quotes);
+
+  var author = authors[Math.floor(Math.random() * authors.length) ];
+
+  var quote = quotes[author];
+
+  document.querySelector("#quote").textContent = quote;
+  document.querySelector("#author").textContent = author;
+};
 
 
-class Worker {
-    constructor(name, surname, rate, days) {
-        this.name = name;
-        this.surname = surname;
-        this.rate = rate;
-        this.days = days;
-    }
-    getSalary() {return this.rate * this.days;}
-}
-const worker = new Worker('Jack', 'Mett', 15, 20);
-console.log(worker.name);
-console.log(worker.surname);
-console.log(worker.rate);
-console.log(worker.days);
-console.log(worker.getSalary());
 
 
 
 
-// №2 модифицируйте класс Worker из предыдущей задачи следующим образом: сделайте все его свойства приватными, а для их чтения
-// сделайте методы-геттеры (подсказка используйте методы Get and Set)
 
-class WorkerTwo {
-    #name;
-    #surname;
-    #rate;
-    #days;
-    constructor(name, surname, rate, days) {
-        this.#name = name;
-        this.#surname = surname;
-        this.#rate = rate;
-        this.#days = days;
-    }
-    getName() {
-        return this.#name;
-    }
-    getSurname() {
-        return this.#surname;
-    }
-    getRate() {
-        return this.#rate;
-    }
-    getDays() {
-        return this.#days;
-    }
-    getSalary(rate, days) {
-        let salary = this.#rate * this.#days;
-        return salary;
-    }
-}
 
-workerTwo = new WorkerTwo('lora', 'Li', '8', '31');
-console.log(workerTwo.getName());
-console.log(workerTwo.getSurname());
-console.log(workerTwo.getRate());
-console.log(workerTwo.getDays());
-console.log(workerTwo.getSalary());
+
+
+const next = document.querySelector('#next')
+const prev = document.querySelector('#prev')
+
+let current = 0
+
+generate = () => {
+    var quotes;
+    quotes = {
+        "- ZZ":
+          '"Ясно только солнце, все остальное вообще не ясно."',
+        "- ZZ":
+          '"Любите искусство в себе, а не себя в искусстве."',
+        "- ZZ":
+          '"Сам себе патронус."',
+        "- ZZ":
+          '"И пусть все сложное станет простым."',
+        "- ZZ":
+          '"Люблю запах возможностей по утром."',
+        "- ZZ":
+          '"Даже если я ошибаюсь, дай мне ошибиться."',
+        "- ZZ":
+          '"Каждый бережет тебя настолько, насколько нуждается в тебе."',
+        "- ZZ":
+          '"В мире полном разочарований, оставайся моим светом."',
+        "- ZZ": '"Не останавливайся, пока не будешь собой гордиться. "',
+        "- ZZ":
+          '"Цените свой покой больше, чем мнение людей."',
+        "- ZZ":
+          '"Пусть будет у вас человек из-за которого хочется меньше сидеть в телефоне."',
+        "- ZZ":
+          '"Где интерес, там и энергия."',
+        "- ZZ":
+          '"Порою нужен сбой в системе, и шаг на ощупь в темноте...  А иногда, побыть не с теми, чтобы, наконец, понять, кто те. "',
+
+ };
+
+    var authors = Object.keys(quotes);
+
+    var author = authors[current];
+
+    var quote = quotes[author];
+
+    document.querySelector("#quote").textContent = quote;
+    document.querySelector("#author").textContent = author;
+};
+
+next.addEventListener('click', () => {
+    current += 1
+    if (current > 12){
+        return false
+    };
+    generate()
+ } );
+prev.addEventListener('click', () => {
+    current -= 1
+    if (current < 0) {
+        return false
+    };
+    generate()
+ } );
 
